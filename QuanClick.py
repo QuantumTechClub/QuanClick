d128640e18abe9d8e2b07990cec3c3936675617c
Steven Leatherbarrow <steven.leatherbarrow@gmail.com>
2024-10-29 16:46:25 +0000

    AI documented Added actions for keyboard and mouse Added install guide

--- QuanClick.py
+++ QuanClick.py
@@ -4,66 +4,80 @@ import digitalio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
-import array
+from adafruit_hid.mouse import Mouse
 
+# Initialize the keyboard and mouse
+keyboard = Keyboard(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
 
-kbd = Keyboard(usb_hid.devices)
+# Define the GPIO pins for the buttons
+button_pins = [board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5]
 
-buttons = [digitalio.DigitalInOut(board.GP0),
-           digitalio.DigitalInOut(board.GP1),
-           digitalio.DigitalInOut(board.GP2)]
-prev = [True, True, True]
+# Create a list of DigitalInOut objects for each button
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
 
-for index,button in enumerate(buttons):
-    button.pull = digitalio.Pull.UP
-    prev[index] = True
-    #print(buttons[index])
+# Initialize a list to store the previous state of each button
+previous_button_states = [True] * len(buttons)
 
-#btn = digitalio.DigitalInOut(board.GP1)
-#btn.pull = digitalio.Pull.UP
+# Configure each button as an input with a pull-up resistor
+for button in buttons:
+    button.switch_to_input(pull=digitalio.Pull.UP)
 
-keys_press = {
-    0: [Keycode.SHIFT, Keycode.S, Keycode.T, Keycode.E],
-    1: [Keycode.S, Keycode.T, Keycode.E, Keycode.E],
-    2: [Keycode.X]
-    }
+# Define actions for each button
+button_actions = {
+    0: {"type": "keyboard", "action": [[Keycode.SHIFT, Keycode.S], Keycode.T, [Keycode.E]]},
+    1: {"type": "keyboard", "action": [Keycode.S, Keycode.T, Keycode.E, Keycode.E]},
+    2: {"type": "keyboard", "action": [[Keycode.WINDOWS, Keycode.E]]},
+    3: {"type": "mouse_move", "action": (20, 0)},  # Move right
+    4: {"type": "mouse_move", "action": (-20, 0)},  # Move left
+    5: {"type": "mouse_click", "action": Mouse.LEFT_BUTTON}
+}
 
-keys_send = {
-    0: [Keycode.SHIFT, Keycode.S, Keycode.T, Keycode.E],
-    1: [Keycode.S, Keycode.T, Keycode.E, Keycode.E],
-    2: [Keycode.X]
-    }
+def perform_keyboard_action(action):
+    for key_code in action:
+        if isinstance(key_code, list):
+            for code in key_code:
+                keyboard.press(code)
+            keyboard.release_all()
+        else:
+            keyboard.press(key_code)
+            keyboard.release(key_code)
 
-keys_release = {
-    0: [[Keycode.SHIFT, Keycode.S], [Keycode.T], [Keycode.E]],
-    1: [Keycode.S, Keycode.T, Keycode.E, Keycode.E],
-    2: [Keycode.X]
-    }
+def perform_mouse_move(movement):
+    mouse.move(x=movement[0], y=movement[1])
 
-print("Start")
-for code in keys_release[0]:
-    print(len(code), ":")
-    for codes in code:
-        print(codes, end=' ')
-    print('')
-print("", "End")
+def perform_mouse_click(button):
+    mouse.click(button)
 
+# Main loop
 while True:
-    for index,button in enumerate(buttons):
-        #print(buttons[index].value)
-#        if buttons[index].value != previous[index]:
-#            previous[index] = not(previous[index])
-#            print(f"button {index} is {previous[index]}")
-        if button.value != prev[index]:
-            prev[index] = not(prev[index])
-            if prev[index]:
-                # button has been released
-                print(f"button {index} is released")
-                for code in keys_release[index]:
-                    kbd.release(code)
-            else:
-                # button has been pressed
-                print(f"button {index} is pressed")
-                for code in keys_press[index]:
-                    kbd.press(code)
+    # Iterate through each button
+    for button_index, button in enumerate(buttons):
+        current_button_state = button.value
 
+        # Check if the button state has changed
+        if current_button_state != previous_button_states[button_index]:
+            # Update the previous state
+            previous_button_states[button_index] = current_button_state
+
+            if current_button_state:  # Button is released (due to pull-up)
+                print(f"Button {button_index} is released")
+
+                # Perform the corresponding action
+                action_info = button_actions.get(button_index)
+                if action_info:
+                    action_type = action_info["type"]
+                    action = action_info["action"]
+
+                    if action_type == "keyboard":
+                        perform_keyboard_action(action)
+                    elif action_type == "mouse_move":
+                        perform_mouse_move(action)
+                    elif action_type == "mouse_click":
+                        perform_mouse_click(action)
+
+            else:  # Button is pressed
+                print(f"Button {button_index} is pressed")
+
+    # Add a small delay to prevent excessive CPU usage
+    time.sleep(0.01)
